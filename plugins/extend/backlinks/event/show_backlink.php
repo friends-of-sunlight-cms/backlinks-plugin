412f3c60640e82e6219fdf29c7d407204392fe6e
jDanek <jirka.danek@jdanek.eu>
2023-09-14 17:02:31 +0200

    Fix ArrayAccess usage

--- plugins/extend/backlinks/event/show_backlink.php
+++ plugins/extend/backlinks/event/show_backlink.php
@@ -4,12 +4,8 @@ use Sunlight\Router;
 use Sunlight\Template;
 use Sunlight\WebState;
 
-/**
- * Event to display page backlink, returns an interaction with the hierarchy.
- * This is a frequently requested remnant of Sunlight CMS 7.x.
- */
 return function (array $args) {
-    if ($this->getConfig()->offsetGet('page_show_backlinks')) {
+    if ($this->getConfig()['page_show_backlinks']) {
         global $_index, $_page;
 
         // pages
